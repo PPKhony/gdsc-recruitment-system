9efb9655bd571cc73771e8afd0a31120b62106d7
praphon <paper2436@gmail.com>
2024-07-11 22:46:00 +0700

    update light theme

--- components/GDSCInterview.tsx
+++ components/GDSCInterview.tsx
@@ -6,24 +6,24 @@ function GDSCCalendar(props: any) {
     (async function () {
       const cal = await getCalApi({});
       cal("ui", {
+        theme: "light",
         styles: { branding: { brandColor: "#000000" } },
         hideEventTypeDetails: false,
         layout: "month_view",
       });
     })();
   }, []);
-  return (   
-      <Cal
-        calLink="praphon.kha/gdsc.tu.interview"
-        style={{ width: "100%" , overflow: "visible" , msOverflowStyle: "none"}}
-        config={{
-          layout: "month_view",
-          email: `${props.data.email}`,
-          name: `${props.data.user_metadata.full_name}`,
-          CandidateID: `${props.data.id}`
-        }}
-      />
-    
+  return (
+    <Cal
+      calLink="https://cal.com/d/dbkzpd5dWmrRV6pvghnoUm/gdsc.tu.interview"
+      style={{ width: "100%", overflow: "visible", msOverflowStyle: "none" }}
+      config={{
+        layout: "month_view",
+        email: `${props.data.email}`,
+        name: `${props.data.user_metadata.full_name}`,
+        CandidateID: `${props.data.id}`,
+      }}
+    />
   );
 }
 
