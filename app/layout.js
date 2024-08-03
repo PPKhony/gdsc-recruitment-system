bc81f4f782b7e1f860a808a652f254f85a4e34e5
PPKhony <work.ppkhon@gmail.com>
2024-08-03 15:50:06 +0700

    test new google analytics without useParam for SPA

--- app/layout.js
+++ app/layout.js
@@ -3,13 +3,9 @@ import Head from "next/head";
 import { Suspense } from "react";
 const RootLayout = ({ children }) => {
   return (
-    <html>
-      <body className="root-layout">
-        <Suspense>
-          <GoogleAnalytics GA_MEASUREMENT_ID="G-P2J3QPMVE4" />
-          {children}
-        </Suspense>
-      </body>
+    <html lang="en">
+      <GoogleAnalytics GA_MEASUREMENT_ID="G-P2J3QPMVE4" />
+      <body className="root-layout">{children}</body>
     </html>
   );
 };
--- utils/GoogleAnalytic.tsx
+++ utils/GoogleAnalytic.tsx
@@ -1,50 +1,27 @@
-"use client";
-import Script from "next/script";
-import { usePathname, useSearchParams } from "next/navigation";
-import { Suspense, useEffect } from "react";
-import { pageview } from "./gTaghelper";
+'use client';
+import Script from 'next/script'
 
-export default function GoogleAnalytics({
-  GA_MEASUREMENT_ID,
-}: {
-  GA_MEASUREMENT_ID: string;
-}) {
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-
-  useEffect(() => {
-    const url = pathname + searchParams.toString();
-
-    pageview(GA_MEASUREMENT_ID, url);
-  }, [pathname, searchParams, GA_MEASUREMENT_ID]);
-  return (
-    <>
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-      />
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
+export default function GoogleAnalytics({GA_MEASUREMENT_ID} : {GA_MEASUREMENT_ID : string}){
+    return (
+        <>
+            <Script strategy="afterInteractive" 
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}/>
+            <Script id='google-analytics' strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                __html: `
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
 
                 gtag('consent', 'default', {
-                    'ad_user_data': 'denied',
-                    'ad_personalization': 'denied',
-                    'ad_storage': 'denied',
                     'analytics_storage': 'denied'
                 });
 
                 gtag('config', '${GA_MEASUREMENT_ID}', {
                     page_path: window.location.pathname,
-                })
+                });
                 `,
-        }}
-      />
-    </>
-  );
-}
+                }}
+            />
+        </>
+)}
