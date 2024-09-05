2c05646e189830023451cb76d01e188a69a0e4b4
PPKhony <work.ppkhon@gmail.com>
2024-09-05 14:27:10 +0700

    hotfix :3

--- app/(auth)/waiting/page.tsx
+++ app/(auth)/waiting/page.tsx
@@ -3,16 +3,16 @@ import { Col, Row } from "react-bootstrap";
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <Row xs={1} lg={2} style={{height: "100dvh"}}>
-        <Col className="d-flex">
-        Test
+      <Row xs={1} lg={2} className="d-flex align-items-center" style={{ height: "100svh" }}>
+        <Col>
+          <img src="/images/maintain-1.gif" width={"100%"} style={{objectFit: "cover"}}  alt="Maintenance" />
         </Col>
-        <Col className="d-flex">
-        <h1>This Website is being maintainance</h1>
-        <h3>Please visit again in 18.00</h3>
+        <Col>
+          <h1>GDSC Recruiting Website is being maintained</h1>
+          <h3>Please visit again at 18:00</h3>
+          <p>Sorry for the inconvenience.</p>
         </Col>
       </Row>
     </main>
   );
 }
-
